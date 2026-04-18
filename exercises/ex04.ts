58d0d7a31f3f8325290e72c1157e3585a84b636d
makoto2025 <aratamakoto2025@gmail.com>
2026-04-17 20:37:34 -0700

    ex04

--- exercises/ex04.ts
+++ exercises/ex04.ts
@@ -5,3 +5,28 @@
 // Create an interface of location with the properties description and the tuple that you created.
 // Print out the interface and coordinates.
 
+type coordinate = [x: number, y: number];
+
+interface Location {
+	description: string;
+	coordinates: coordinate;
+}
+
+const location1: Location = {
+	description: "Paris",
+	coordinates: [48.8584, 2.2945],
+};
+
+const location2: Location = {
+	description: "New York",
+	coordinates: [40.6892, -74.0445],
+};
+
+const locations: Location[] = [location1, location2];
+
+locations.forEach((location) => {
+	console.log(`Description: ${location.description}`);
+	console.log(
+		`Coordinates: X= ${location.coordinates[0]}, Y= ${location.coordinates[1]}`,
+	);
+});
