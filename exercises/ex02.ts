e927c84864bc4235b4e19a22b56ad73bcfa40d81
makoto2025 <aratamakoto2025@gmail.com>
2026-04-17 18:41:42 -0700

    ex02

---
+++ exercises/ex02.ts
@@ -0,0 +1,14 @@
+// Exercise 2: Create and Manipulate an Array
+
+// Create an array called fruits containing strings representing different fruits.
+// Add a new fruit to the array.
+// Print out the list of fruits.
+
+let fruits: string[];
+fruits = ["orange", "banana", "cherry"];
+
+console.log(fruits);
+
+fruits.push("apple")
+
+console.log(fruits);
